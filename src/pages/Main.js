429b40d8a1ed2091847bb3e6ca04da5c8da26dc7
Hou <houchongchan@gmail.com>
2022-02-15 22:05:03 -0500

    added video

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,19 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import About from './About';
 import Header from '../components/Header';
 import Newsletter from '../components/Newsletter';
+import shopping from '../img/shopping.mp4';
 
 const Main = () => {
     const Slogan = () => {
         return(
             <>
                 <Header text = 'Top Online Shopping Brand' />
-                <Link to = '/products'>
-                    <button className = 'clear-container-blue'>
-                        Shopping
-                    </button>
-                </Link>
+                <div className = 'hero'>
+                    <video  src = {shopping} autoPlay loop muted type = 'video/mp4'><a href="https://www.vecteezy.com/video/1625419-clouds-and-sky-animated-background"></a></video>
+                    <Link to = '/products'>
+                        <button className = 'btn-shopping'>
+                            Shop Here
+                        </button>
+                    </Link>
+                </div>
                 <Header text = 'Featured Products' />
             </>
 
